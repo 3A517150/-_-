a48d75a213c332a2326d93179a5ee1908912a8a8
YeShiouWei <s2410532005@gms.nutc.edu.tw>
2020-10-04 10:02:52 +0800

    10/4 修改: Camera_Video.py 跟 VideoToHaar.py 功能結合

--- Camera_Video.py
+++ Camera_Video.py
@@ -1,6 +1,5 @@
 import cv2
 import time
-import VideoToHaar
 # ---- 攝影機攝取影像尺寸
 _CAMERA_WIDTH = 640
 _CAMERA_HEIGHT = 480
@@ -22,9 +21,34 @@ write_Camera = 0 #判斷寫入模式
 video_counter = 0 #計算影片數量
 time.sleep(2)
 
+# ---- Haar
+faceCascade = cv2.CascadeClassifier(r'/Users/yeshiouwei/opt/anaconda3/pkgs/libopencv-3.4.2-h7c891bd_1/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+
+def Haar(gray):
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(100, 100),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    return faces
+
 while (camera.isOpened()):
+
+    # ---- 畫面讀取
     ret, frame = camera.read()
+
+    # ---- 畫面翻轉
     frame = cv2.flip(frame, 1)
+    gray = frame.copy()
+
+    # ---- Haar 人臉偵測 + 人臉標記
+    faces = Haar(frame)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
     sec = time.strftime("%S", time.localtime())
     sec = int(sec)
     if ret == True:
